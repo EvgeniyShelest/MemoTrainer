40dc31780c458a81dbed8abe82d0cf6a09f67e0a
Evgeniy Shelest <zhck12@gmail.com>
2022-09-12 13:37:29 +0300

    Init

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const shuffle = function (o) {
+  for (
+    var j, x, i = o.length;
+    i;
+    j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
+};
+const values = shuffle([...Array(8).keys()].concat([...Array(8).keys()]));
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>working on your app!</Text>
+      <View style={styles.row}>
+        {[...values.keys()].map((index) => (
+          <TouchableOpacity key={index} onPress={() => alert(index)}>
+            <View style={styles.cell}>
+              <Text>{values[index]}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: "#cff",
+    width: 350,
+    padding: 15,
+  },
+  cell: {
+    backgroundColor: "#fcf",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 75,
+    height: 75,
+    margin: 2,
+  }
 });
