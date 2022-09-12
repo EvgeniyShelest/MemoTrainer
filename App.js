261fdf9191567579142a1d3de5aff9d4a5f8e8ef
Evgeniy Shelest <zhck12@gmail.com>
2022-09-12 17:47:06 +0300

    Worked mocked model

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import shuffle from "./utils/shuffle"
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import shuffle from "./utils/shuffle";
 
 const values = shuffle([...Array(8).keys()].concat([...Array(8).keys()]));
 
 export default function App() {
+  const [clicked, setClicked] = useState([]);
+  const [solvedIndexes, setSolvedIndexes] = useState([]);
+  const [prevIndex, setPrevIndex] = useState(null);
+  const handleItemPress = (index) => {
+    if (index === prevIndex || solvedIndexes.includes(index)) return;
+    if (values[prevIndex] === values[index]) {
+      setSolvedIndexes((previousState) => [...previousState, prevIndex, index]);
+      setPrevIndex(null);
+    } else {
+      setPrevIndex(index);
+    }
+    setClicked((previousState) => [...previousState, index]);
+  };
+  const resetHandler = () => {
+    setClicked([]);
+    setSolvedIndexes([]);
+    setPrevIndex(null);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>working on your app!</Text>
+      <Text>Steps: {clicked.length}</Text>
+      <Text>prevIndex: {prevIndex}</Text>
+      <View style={styles.buttonRow}>
+        <Button title="Start" onPress={() => startHandler()} />
+        <Button title="Reset" onPress={resetHandler} />
+      </View>
       <View style={styles.row}>
         {[...values.keys()].map((index) => (
-          <TouchableOpacity key={index} onPress={() => alert(index)}>
-            <View style={styles.cell}>
+          <TouchableOpacity key={index} onPress={() => handleItemPress(index)}>
+            <View
+              style={[
+                styles.cell,
+                solvedIndexes.includes(index) && styles.solved,
+              ]}
+            >
               <Text>{values[index]}</Text>
             </View>
           </TouchableOpacity>
@@ -29,6 +59,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  buttonRow: {
+    width: 150,
+    margin: 20,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
   row: {
     flexDirection: "row",
     flexWrap: "wrap",
@@ -43,5 +79,8 @@ const styles = StyleSheet.create({
     width: 75,
     height: 75,
     margin: 2,
-  }
+  },
+  solved: {
+    backgroundColor: "#aaf",
+  },
 });
